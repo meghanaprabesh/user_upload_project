9968e6044381620204b8656ff79b7c6d3a43bb0c
meghana prabesh <meghana2595@gmail.com>
2024-03-27 15:33:47 +1100

    addding readme file for executing commands

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Step 1 - Create user database in mysql (user.sql file added in mysql repository)
+Step2 - Clone the repository from git (https://github.com/meghanaprabesh/user_upload_project)
+Step3 - Update the database uname password and database name 
+Step4 -  To run the output
+Commands to execute using command prompt
+
+To create the table inside database
+php user_upload.php --create
+To upload csv file into the table
+php user_upload.php --file users.csv
---
+++ user.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2024 at 05:12 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `email`) VALUES
+(1, 'John', 'Smith', 'jsmith@gmail.com'),
+(2, 'Hamish', 'Jones', 'ham@seek.com'),
+(3, 'Phil', 'Carry', 'phil@open.edu.au'),
+(4, 'Johnny', 'Ohare', 'john@yahoo.com.au'),
+(5, 'William', 'Smythe', 'happy@ent.com.au'),
+(7, 'Sam', 'Walters', 'sam!@walters.org'),
+(8, 'Daley', 'Thompson', 'daley@yahoo.co.nz'),
+(9, 'Kevin', 'Ruley', 'kevin.ruley@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
